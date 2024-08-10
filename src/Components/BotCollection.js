11e9e7af7ef8154760ff1f955a3322638250ba28
Abel Koech <koechabel161@gmail.com>
2024-08-10 20:55:44 +0300

    filter bot by class

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -8,6 +8,8 @@ const BotCollection = () => {
     const [bots, setBots]=useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [error, setError] = useState(null)
+    const [filteredBots, setFilteredBots]= useState([])
+    const [selectedFilters, setSelectedFilters]= useState([])
   
   
 
@@ -21,6 +23,7 @@ const BotCollection = () => {
           const data = await res.json()
           //console.log(data)
           setBots(data)
+          setFilteredBots(data)
         } catch (error) {
           
         }finally {
@@ -29,20 +32,54 @@ const BotCollection = () => {
       }
       fetchBots()
     },[])
+     
+    useEffect(()=>{
+      if(selectedFilters.length === 0){
+        setFilteredBots(bots)
+      }else {
+        const newFilteredBots = bots.filter(bot => selectedFilters.includes(bot.bot_class))
+        setFilteredBots(newFilteredBots)
+      }
+    }, [selectedFilters, bots]) 
+
+    const handleFilterChange = (e) =>{
+      const value = e.target.value
+      setSelectedFilters(prevFilters => 
+        prevFilters.includes(value)
+        ? prevFilters.filter(filter => filter !== value) :
+        [...prevFilters, value]
+      )
+    }
+
     if(isLoading){
         return <p>Loading...</p>
     }
     if(error){
         return <p>Error: {error}</p>
     }
+    const uniqueClasses = [...new Set(bots.map(bot =>bot.bot_class))]
   return (
     
     <div className='collection'>
         <h1>Bot Battlr</h1>
+    <div className='filters'>
+      |<h2>Filter by Class:</h2>
+      {uniqueClasses.map(botclass => (
+        <label key={botclass}>
+        <input 
+        type='checkbox'
+        value={botclass}
+        checked={selectedFilters.includes(botclass)}
+        onChange={handleFilterChange}/>
+        {botclass}
+        </label>
+      ))}
+    </div>
         <BotArmy />
         <h1>Available Bots</h1>
+        
     <div className='bot-collection'>
-    {bots.map(bot=>(
+    {filteredBots.map(bot=>(
         <BotCard key={bot.id} bot={bot} handleEnlist={handleEnlist}/>
       ))}
     </div>
